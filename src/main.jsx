608afb3ee444af41d730285511d9a22f99053621
naeemmiah70 <naeem@braina.live>
2025-07-29 23:47:18 +0600

    js bundle added to the bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import '@fontsource/poppins/400.css';
 import '@fontsource/mulish/400.css';
 import '@fontsource/archivo/400.css';
